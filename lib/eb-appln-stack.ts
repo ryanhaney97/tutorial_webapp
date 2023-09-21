623bb59cfa13e48f14e7f2804be5aedb64839908
yoshiquest <yoshiquest@gmail.com>
2023-09-21 13:48:37 -0500

    Fixed Amazon Linux version

--- lib/eb-appln-stack.ts
+++ lib/eb-appln-stack.ts
@@ -88,7 +88,7 @@ const optionSettingProperties: elasticbeanstalk.CfnEnvironment.OptionSettingProp
 const elbEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
     environmentName: props?.envName ?? "MyWebAppEnvironment",
     applicationName: app.applicationName || appName,
-    solutionStackName: '64bit Amazon Linux 2 v5.8.0 running Node.js 18',
+    solutionStackName: '64bit Amazon Linux 2 v5.8.5 running Node.js 18',
     optionSettings: optionSettingProperties,
     versionLabel: appVersionProps.ref,
 });
